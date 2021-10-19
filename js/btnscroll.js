32d744f1913e86a6eda9f97c8f6c7eb977ac4ae2
Ahmadsabili <ahmadsabili0081@gmail.com>
2021-10-19 13:37:37 +0700

    initial commit

--- js/btnscroll.js
+++ js/btnscroll.js
@@ -1,11 +1,10 @@
 window.addEventListener('scroll',function(){
     const btn = document.querySelector('.btn');
     btn.classList.toggle('scroll', window.scrollY > 300);
-})
+});
 
-var btn = doocument.getElementById('btn');
 function btn(){
     document.body.scrollTop ="0";
 
     document.documentElement.scrollTop="0";
-}
+};
---
+++ js/modal.js
@@ -0,0 +1,8 @@
+function toggle(){
+    var blur  = document.getElementById('blur');
+    blur.classList.toggle('active');
+}
+function toggle2(){
+    var blur  = document.getElementById('blur2');
+    blur.classList.toggle('active2');
+}
